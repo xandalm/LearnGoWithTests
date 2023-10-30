052f1c063d8de40b346adbb24ef9de3366b7c9fb
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-10-30 15:04:51 -0300

    v2

--- GoFundamentals/structs-methods-interfaces/shapes.go
+++ GoFundamentals/structs-methods-interfaces/shapes.go
@@ -1,13 +1,35 @@
 package main
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width, Height float64
 }
 
-func Perimeter(rect Rectangle) float64 {
-	return 2 * (rect.Width + rect.Height)
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-func Area(rect Rectangle) float64 {
-	return rect.Width * rect.Height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base, Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
+}
+
+func Perimeter(rect Rectangle) float64 {
+	return 2 * (rect.Width + rect.Height)
 }
--- GoFundamentals/structs-methods-interfaces/shapes_test.go
+++ GoFundamentals/structs-methods-interfaces/shapes_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -13,11 +15,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g, want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
