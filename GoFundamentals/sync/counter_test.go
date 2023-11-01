8ab83eba0622acfd39973579882d924212ffe767
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-01 12:36:38 -0300

    v2

--- GoFundamentals/sync/counter_test.go
+++ GoFundamentals/sync/counter_test.go
@@ -1,19 +1,41 @@
 package counter
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
 	})
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 100
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+// Note: Changed `got` to *Counter type because Mutex must not be copied after first use.
+// These errors can be seen with `go vet` command line
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
