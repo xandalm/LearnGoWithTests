39cfd5bba665339effbef5ab67bc36cdf11edd51
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-02 12:51:16 -0300

    v5

--- GoFundamentals/property-based-tests/numeral.go
+++ GoFundamentals/property-based-tests/numeral.go
@@ -60,17 +60,14 @@ func ConvertToArabic(roman string) int {
 			// build the two character string
 			potentialNumber := string([]byte{symbol, nextSymbol})
 
-			// get the value of the two character string
-			value := allRomanNumerals.ValueOf(potentialNumber)
-
-			if value != 0 {
+			if value := allRomanNumerals.ValueOf(potentialNumber); value != 0 {
 				total += value
 				i++ // move past this character too for the next loop
 			} else {
 				total++
 			}
 		} else {
-			total++
+			total += allRomanNumerals.ValueOf(string([]byte{symbol}))
 		}
 	}
 	return total
