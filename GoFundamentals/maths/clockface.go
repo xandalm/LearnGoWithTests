f9b9b7d09e5393d061b1e2ef40dc264e2ad486fc
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-02 16:12:09 -0300

    v1

---
+++ GoFundamentals/maths/clockface.go
@@ -0,0 +1,14 @@
+package clockface
+
+import "time"
+
+// A Point represents a two-dimensional Cartesian coordinate
+type Point struct {
+	X, Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
