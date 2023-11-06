6f3123d56de0e941c388d782bf385953e68c9a2e
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-06 12:19:25 -0300

    v12/refactor

--- GoFundamentals/maths/clockface.go
+++ GoFundamentals/maths/clockface.go
@@ -10,6 +10,15 @@ type Point struct {
 	X, Y float64
 }
 
+const (
+	secondsInHalfClock = 30
+	secondsInClock     = 2 * secondsInHalfClock
+	minutesInHalfClock = 30
+	minutesInClock     = 2 * minutesInHalfClock
+	hoursInHalfClock   = 6
+	hoursInClock       = 2 * hoursInHalfClock
+)
+
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
@@ -21,7 +30,7 @@ func SecondHand(t time.Time) Point {
 }
 
 func secondsInRadians(t time.Time) float64 {
-	return (math.Pi / (30 / (float64(t.Second()))))
+	return (math.Pi / (secondsInHalfClock / (float64(t.Second()))))
 }
 
 func secondHandPoint(t time.Time) Point {
@@ -29,8 +38,8 @@ func secondHandPoint(t time.Time) Point {
 }
 
 func minutesInRadians(t time.Time) float64 {
-	return (secondsInRadians(t) / 60) +
-		(math.Pi / (30 / float64(t.Minute())))
+	return (secondsInRadians(t) / minutesInClock) +
+		(math.Pi / (minutesInHalfClock / float64(t.Minute())))
 }
 
 func minuteHandPoint(t time.Time) Point {
@@ -38,7 +47,7 @@ func minuteHandPoint(t time.Time) Point {
 }
 
 func hoursInRadians(t time.Time) float64 {
-	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
+	return (minutesInRadians(t) / hoursInClock) + (math.Pi / (hoursInHalfClock / float64(t.Hour()%hoursInClock)))
 }
 
 func hourHandPoint(t time.Time) Point {
