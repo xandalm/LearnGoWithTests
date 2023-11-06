fa8f6b74cc6a891ce5f9ccbc7a6e1bfb2828adf8
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-06 11:24:35 -0300

    v8/refactor

--- GoFundamentals/maths/clockface.go
+++ GoFundamentals/maths/clockface.go
@@ -25,10 +25,7 @@ func secondsInRadians(t time.Time) float64 {
 }
 
 func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
-	x := math.Sin(angle)
-	y := math.Cos(angle)
-	return Point{x, y}
+	return angleToPoint(secondsInRadians(t))
 }
 
 func minutesInRadians(t time.Time) float64 {
@@ -37,7 +34,10 @@ func minutesInRadians(t time.Time) float64 {
 }
 
 func minuteHandPoint(t time.Time) Point {
-	angle := minutesInRadians(t)
+	return angleToPoint(minutesInRadians(t))
+}
+
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 	return Point{x, y}
