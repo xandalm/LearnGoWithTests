8d1bc6c696f408a5b9effbf5b70345483113f69a
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-06 15:20:37 -0300

    v4/refactor

--- GoFundamentals/reading-files/blogposts.go
+++ GoFundamentals/reading-files/blogposts.go
@@ -53,22 +53,19 @@ func newPost(postFile io.Reader) (Post, error) {
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
-	title := readMetaLine(titleSeparator)
-	description := readMetaLine(descriptionSeparator)
-	tags := strings.Split(readMetaLine(tagsSeparator), ", ")
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
 
-	scanner.Scan() // ignore a line
-
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line (---)
 	buf := bytes.Buffer{}
 	for scanner.Scan() {
 		fmt.Fprintln(&buf, scanner.Text())
 	}
-	body := strings.TrimSuffix(buf.String(), "\n")
-
-	return Post{
-		Title:       title,
-		Description: description,
-		Tags:        tags,
-		Body:        body,
-	}, nil
+	return strings.TrimSuffix(buf.String(), "\n")
 }
