89175e5e23862897cd9b7b4dba247f44101c164d
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-07 16:36:43 -0300

    v5

--- GoFundamentals/generics/stack.go
+++ GoFundamentals/generics/stack.go
@@ -1,43 +1,24 @@
 package generics
 
-type StackOfInts struct {
-	values []int
+type StackOfInts = Stack
+type StackOfStrings = Stack
+
+type Stack struct {
+	values []interface{}
 }
 
-func (s *StackOfInts) Push(value int) {
+func (s *Stack) Push(value interface{}) {
 	s.values = append(s.values, value)
 }
 
-func (s *StackOfInts) IsEmpty() bool {
+func (s *Stack) IsEmpty() bool {
 	return len(s.values) == 0
 }
 
-func (s *StackOfInts) Pop() (int, bool) {
+func (s *Stack) Pop() (interface{}, bool) {
 	if s.IsEmpty() {
-		return 0, false
-	}
-
-	index := len(s.values) - 1
-	el := s.values[index]
-	s.values = s.values[:index]
-	return el, true
-}
-
-type StackOfStrings struct {
-	values []string
-}
-
-func (s *StackOfStrings) Push(value string) {
-	s.values = append(s.values, value)
-}
-
-func (s *StackOfStrings) IsEmpty() bool {
-	return len(s.values) == 0
-}
-
-func (s *StackOfStrings) Pop() (string, bool) {
-	if s.IsEmpty() {
-		return "", false
+		var zero interface{}
+		return zero, false
 	}
 
 	index := len(s.values) - 1
