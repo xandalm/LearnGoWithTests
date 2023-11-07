f08f23712b9eca0bf7e0041c0abc1ef676f3bb22
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-07 11:59:18 -0300

    v1/refactor

--- GoFundamentals/templating/blogrenderer.go
+++ GoFundamentals/templating/blogrenderer.go
@@ -1,30 +1,21 @@
 package blogrenderer
 
 import (
-	"fmt"
+	"html/template"
 	"io"
 )
 
+const (
+	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+)
+
 func Render(w io.Writer, p Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Description)
+	templ, err := template.New("blog").Parse(postTemplate)
 	if err != nil {
 		return err
 	}
 
-	_, err = fmt.Fprint(w, "Tags: <ul>")
-	if err != nil {
-		return err
-	}
-
-	for _, tag := range p.Tags {
-		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
-		if err != nil {
-			return err
-		}
-	}
-
-	_, err = fmt.Fprint(w, "</ul>")
-	if err != nil {
+	if err := templ.Execute(w, p); err != nil {
 		return err
 	}
 
