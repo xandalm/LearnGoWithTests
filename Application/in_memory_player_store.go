7b459b6fccd941e4733add181d1b14ab1898e83e
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-21 15:03:04 -0300

    v6/refactor

--- Application/in_memory_player_store.go
+++ Application/in_memory_player_store.go
@@ -1,17 +1,27 @@
 package main
 
+import "sync"
+
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{
+		sync.RWMutex{},
+		map[string]int{},
+	}
 }
 
 type InMemoryPlayerStore struct {
+	mu    sync.RWMutex
 	store map[string]int
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
 	i.store[name]++
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
 	return i.store[name]
 }
