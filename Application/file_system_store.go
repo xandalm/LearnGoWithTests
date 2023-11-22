0384e72e450154874091b2e6f665bd2d575f2fe7
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-22 17:23:04 -0300

    v4/refactor

--- Application/file_system_store.go
+++ Application/file_system_store.go
@@ -9,30 +9,25 @@ type FileSystemPlayerStore struct {
 	database io.ReadWriteSeeker // because we need to move to start of the file content (and we need to write into file now)
 }
 
-func (f *FileSystemPlayerStore) GetLeague() []Player {
+func (f *FileSystemPlayerStore) GetLeague() League {
 	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
-		}
+	player := f.GetLeague().Find(name)
+	if player != nil {
+		return player.Wins
 	}
-	return wins
+	return 0
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	league := f.GetLeague()
-	for i, player := range league {
-		if player.Name == name {
-			league[i].Wins++
-			break
-		}
+	player := league.Find(name)
+	if player != nil {
+		player.Wins++
 	}
 	f.database.Seek(0, 0)
 	json.NewEncoder(f.database).Encode(league)
