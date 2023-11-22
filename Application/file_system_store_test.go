dc66b669716195e15483b0d2393073d137c77a00
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-22 16:50:24 -0300

    v3

--- Application/file_system_store_test.go
+++ Application/file_system_store_test.go
@@ -25,4 +25,18 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+	t.Run("get player store", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Chris")
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d but want %d", got, want)
+		}
+	})
 }
