e97a37719cbe98d6d8ed13d04f191600f526466b
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-23 15:10:36 -0300

    v4/refactor

--- Application/cmd/webserver/main.go
+++ Application/cmd/webserver/main.go
@@ -3,24 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 	"poker"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, closeFunc, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
+	defer closeFunc()
 
 	server := poker.NewPlayerServer(store)
 
