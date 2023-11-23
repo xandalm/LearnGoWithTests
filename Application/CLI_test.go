be98be9e57d76914bf0385347e633823cd76ef66
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-11-23 14:22:20 -0300

    v3

--- Application/CLI_test.go
+++ Application/CLI_test.go
@@ -1,13 +1,24 @@
 package poker
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+	cli := &CLI{playerStore, in}
 	cli.PlayPoker()
 
 	if len(playerStore.winCalls) != 1 {
 		t.Fatalf("expected a win call but didn't get any")
+	}
+
+	got := playerStore.winCalls[0]
+	want := "Chris"
+
+	if got != want {
+		t.Errorf("didn't record correct winner, got %q but want %q", got, want)
 	}
 }
